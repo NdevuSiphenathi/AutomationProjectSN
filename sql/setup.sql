c6f5129762e57b01d653d4dfa7be6615fe99450f
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-03-27 15:11:48 +0200

    Update setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,28 +1,17 @@
--- Step 1: Create AutoTest_SN27March database via stored procedure
+-- Step 1: Create AutoTestSiphenathi database
 USE master;
 GO
-IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'CreateAutoTest_SN27March')
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AutoTestSiphenathi')
 BEGIN
-    DROP PROCEDURE CreateAutoTest_SN27March;
+    CREATE DATABASE AutoTestSiphenathi;
+    PRINT 'Database AutoTestSiphenathi created.';
 END
-GO
-CREATE PROCEDURE CreateAutoTest_SN27March
-AS
-BEGIN
-    IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AutoTest_SN27March')
-    BEGIN
-        CREATE DATABASE AutoTest_SN27March;
-        PRINT 'Database AutoTest_SN27March created.';
-    END
-    ELSE
-        PRINT 'Database AutoTest_SN27March already exists.';
-END;
-GO
-EXEC CreateAutoTest_SN27March;
+ELSE
+    PRINT 'Database AutoTestSiphenathi already exists.';
 GO
 
--- Step 2: Switch to AutoTest_SN27March
-USE AutoTest_SN27March;
+-- Step 2: Switch to AutoTestSiphenathi
+USE AutoTestSiphenathi;
 GO
 
 -- Step 3: Create user table
