4dbd96470466dbf52a5e9dbef5c68b89f8a2852d
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-06-20 13:23:08 +0200

    Update JobScript.sql

--- sql/JobScript.sql
+++ sql/JobScript.sql
@@ -1,46 +1,46 @@
 USE [msdb];
 GO
 
--- Drop job if it exists
+-- Remove existing job
 IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'@jobName')
 BEGIN
     EXEC msdb.dbo.sp_delete_job @job_name = N'@jobName';
 END
 GO
 
--- Create new job
-DECLARE @jobId UNIQUEIDENTIFIER;
+-- Create job
+DECLARE @jobId BINARY(16);
 EXEC msdb.dbo.sp_add_job 
     @job_name = N'@jobName',
     @enabled = 1,
     @notify_level_eventlog = 2,
-    @description = N'SSIS job to run @jobName every 60 seconds',
+    @delete_level = 0,
+    @description = N'Scheduled job to execute SSIS package: @jobName every 1 minute',
     @category_name = N'[Uncategorized (Local)]',
-    @owner_login_name = N'sa',
+    @owner_login_name = N'$env:DB_USER',
     @job_id = @jobId OUTPUT;
 GO
 
--- Add job step
+-- Add job step to run SSIS package
 EXEC msdb.dbo.sp_add_jobstep 
     @job_name = N'@jobName',
     @step_name = N'Run SSIS Package',
     @subsystem = N'SSIS',
-    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName" /SERVER "LAPTOP-ATT0UPK9" /ENVREFERENCE 1',
-    @on_success_action = 1,
-    @on_fail_action = 2,
+    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName.dtsx" /SERVER "$env:DB_SERVER" /ENVREFERENCE 0 /ISSERVER',
     @database_name = N'master',
-    @output_file_name = N'C:\SSISLogs\@jobName.log';  -- Ensure Agent has access
+    @on_success_action = 1,
+    @on_fail_action = 2;
 GO
 
--- Add schedule (every 1 minute)
+-- Single schedule: starts at 00:00:00, every 1 minute
 EXEC msdb.dbo.sp_add_jobschedule 
     @job_name = N'@jobName',
     @name = N'RunEveryMinute',
     @enabled = 1,
-    @freq_type = 4,  -- Daily
+    @freq_type = 4,  -- daily
     @freq_interval = 1,
-    @freq_subday_type = 2,  -- Seconds
-    @freq_subday_interval = 60,  -- Every 60s
+    @freq_subday_type = 4, -- minutes
+    @freq_subday_interval = 1, -- every 1 minute
     @active_start_time = 000000;
 GO
 
