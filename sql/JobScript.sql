9e886f06ba747f926f32e659ed43f22a2058e8c6
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-06-20 13:11:23 +0200

    Update JobScript.sql

--- sql/JobScript.sql
+++ sql/JobScript.sql
@@ -1,59 +1,47 @@
 USE [msdb];
 GO
 
--- Remove existing job
+-- Drop job if it exists
 IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'@jobName')
 BEGIN
     EXEC msdb.dbo.sp_delete_job @job_name = N'@jobName';
 END
 GO
 
--- Create job
-DECLARE @jobId BINARY(16);
+-- Create new job
+DECLARE @jobId UNIQUEIDENTIFIER;
 EXEC msdb.dbo.sp_add_job 
     @job_name = N'@jobName',
     @enabled = 1,
     @notify_level_eventlog = 2,
-    @delete_level = 0,
-    @description = N'Scheduled job to execute SSIS package: @jobName every 30 seconds',
+    @description = N'SSIS job to run @jobName every 60 seconds',
     @category_name = N'[Uncategorized (Local)]',
     @owner_login_name = N'sa',
     @job_id = @jobId OUTPUT;
 GO
 
--- Add job step to run SSIS package
+-- Add job step
 EXEC msdb.dbo.sp_add_jobstep 
     @job_name = N'@jobName',
     @step_name = N'Run SSIS Package',
     @subsystem = N'SSIS',
-    @command = N'/SQL "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName" /SERVER "LAPTOP-ATT0UPK9" /ENVREFERENCE 0 /ISSERVER',
-    @database_name = N'master',
+    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName" /SERVER "LAPTOP-ATT0UPK9" /ENVREFERENCE 1',
     @on_success_action = 1,
-    @on_fail_action = 2;
+    @on_fail_action = 2,
+    @database_name = N'master',
+    @output_file_name = N'C:\SSISLogs\@jobName.log';  -- Ensure Agent has access
 GO
 
--- First schedule: starts at 00:00:00
+-- Add schedule (every 1 minute)
 EXEC msdb.dbo.sp_add_jobschedule 
     @job_name = N'@jobName',
-    @name = N'RunEveryMinute_0',
+    @name = N'RunEveryMinute',
     @enabled = 1,
-    @freq_type = 4,  -- daily
+    @freq_type = 4,  -- Daily
     @freq_interval = 1,
-    @freq_subday_type = 2, -- seconds
-    @freq_subday_interval = 60, -- every 60 seconds
+    @freq_subday_type = 2,  -- Seconds
+    @freq_subday_interval = 60,  -- Every 60s
     @active_start_time = 000000;
-GO
-
--- Second schedule: starts at 00:00:30
-EXEC msdb.dbo.sp_add_jobschedule 
-    @job_name = N'@jobName',
-    @name = N'RunEveryMinute_30',
-    @enabled = 1,
-    @freq_type = 4,  -- daily
-    @freq_interval = 1,
-    @freq_subday_type = 2, -- seconds
-    @freq_subday_interval = 60,
-    @active_start_time = 000030;
 GO
 
 -- Attach job to current server
