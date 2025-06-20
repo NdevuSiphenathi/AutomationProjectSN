476a26c911169f6e0a0462ccc8f8087bb9ea57af
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-06-20 10:41:58 +0200

    Update JobScript.sql

--- sql/JobScript.sql
+++ sql/JobScript.sql
@@ -1,42 +1,63 @@
-USE msdb;
+USE [msdb];
 GO
 
--- Delete existing job if it exists
-IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'DeploySSISPackageJob')
-    EXEC msdb.dbo.sp_delete_job @job_name = N'DeploySSISPackageJob';
+-- Remove existing job
+IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'@jobName')
+BEGIN
+    EXEC msdb.dbo.sp_delete_job @job_name = N'@jobName';
+END
 GO
 
--- Create new job
-EXEC msdb.dbo.sp_add_job
-    @job_name = N'DeploySSISPackageJob',
+-- Create job
+DECLARE @jobId BINARY(16);
+EXEC msdb.dbo.sp_add_job 
+    @job_name = N'@jobName',
     @enabled = 1,
-    @description = N'Job to run SSIS package ProjectPackages',
-    @owner_login_name = N'sa';
+    @notify_level_eventlog = 2,
+    @delete_level = 0,
+    @description = N'Scheduled job to execute SSIS package: @jobName every 30 seconds',
+    @category_name = N'[Uncategorized (Local)]',
+    @owner_login_name = N'sa',
+    @job_id = @jobId OUTPUT;
 GO
 
--- Add job step to execute SSIS package
-EXEC msdb.dbo.sp_add_jobstep
-    @job_name = N'DeploySSISPackageJob',
-    @step_name = N'Execute SSIS Package',
+-- Add job step to run SSIS package
+EXEC msdb.dbo.sp_add_jobstep 
+    @job_name = N'@jobName',
+    @step_name = N'Run SSIS Package',
     @subsystem = N'SSIS',
-    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\ProjectPackages\ProjectPackages.dtsx" /SERVER "LAPTOP-ATT0UPK9\MSSQLSERVER04"',
-    @database_name = N'master';
+    @command = N'/SQL "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName" /SERVER "LAPTOP-ATT0UPK9" /ENVREFERENCE 0 /ISSERVER',
+    @database_name = N'master',
+    @on_success_action = 1,
+    @on_fail_action = 2;
 GO
 
--- Add job schedule (e.g., every 30 seconds - Note: Minimum interval is 1 minute, adjusted to 1 minute)
-EXEC msdb.dbo.sp_add_jobschedule
-    @job_name = N'DeploySSISPackageJob',
-    @name = N'DailySchedule',
+-- First schedule: starts at 00:00:00
+EXEC msdb.dbo.sp_add_jobschedule 
+    @job_name = N'@jobName',
+    @name = N'RunEveryMinute_0',
     @enabled = 1,
-    @freq_type = 4, -- Daily
+    @freq_type = 4,  -- daily
     @freq_interval = 1,
-    @freq_subday_type = 2, -- Minutes
-    @freq_subday_interval = 1, -- Every 1 minute (closest to 30 seconds due to SQL Server Agent limitation)
-    @active_start_time = 020000; -- 2:00 AM
+    @freq_subday_type = 2, -- seconds
+    @freq_subday_interval = 60, -- every 60 seconds
+    @active_start_time = 000000;
 GO
 
--- Add job to server
-EXEC msdb.dbo.sp_add_jobserver
-    @job_name = N'DeploySSISPackageJob',
-    @server_name = N'(local)';
+-- Second schedule: starts at 00:00:30
+EXEC msdb.dbo.sp_add_jobschedule 
+    @job_name = N'@jobName',
+    @name = N'RunEveryMinute_30',
+    @enabled = 1,
+    @freq_type = 4,  -- daily
+    @freq_interval = 1,
+    @freq_subday_type = 2, -- seconds
+    @freq_subday_interval = 60,
+    @active_start_time = 000030;
+GO
+
+-- Attach job to current server
+EXEC msdb.dbo.sp_add_jobserver 
+    @job_name = N'@jobName',
+    @server_name = N'(LOCAL)';
 GO
