7e5775595ef82516e12cb4a7d4ef25d6d2ad857f
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-06-20 14:39:12 +0200

    Update JobScript.sql

--- sql/JobScript.sql
+++ sql/JobScript.sql
@@ -26,7 +26,7 @@ EXEC msdb.dbo.sp_add_jobstep
     @job_name = N'@jobName',
     @step_name = N'Run SSIS Package',
     @subsystem = N'SSIS',
-    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName.dtsx" /SERVER "BENT-DB_SERVER"',
+    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\ProjectPackages\@jobName" /SERVER "BENT-DB_SERVER"',
     @database_name = N'master',
     @on_success_action = 1,
     @on_fail_action = 2;
